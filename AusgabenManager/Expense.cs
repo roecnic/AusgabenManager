c861ec7fc4362e607d85376d86356b7d2ebeedbf
Nicolas Röchter <rr@tobit.com>
2017-08-17 10:07:58 +0200

    Init class Expense, add fields and constructor

---
+++ AusgabenManager/Expense.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AusgabenManager {
+    class Expense {
+        private string fCategoryName;
+
+        public string CategoryName {
+            get { return fCategoryName; }
+            set { fCategoryName = value; }
+        }
+
+        public int fCategoryID;
+
+        public int CategoryID {
+            get { return fCategoryID; }
+        }
+
+        public Expense(int pID, string pCategoryName) {
+            fCategoryID = pID;
+            fCategoryName = pCategoryName;
+        }
+    }
+}
