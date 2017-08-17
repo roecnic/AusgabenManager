a4cad876189c9a5a52d404a20e07b948fc7b0fbe
Nicolas Röchter <rr@tobit.com>
2017-08-17 11:01:08 +0200

    Init class cCategory

---
+++ AusgabenManager/cCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AusgabenManager {
+    class cCategory {
+        private string fName;
+
+        public string CategoryName {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        private int fID;
+
+        public int ID {
+            get { return fID; }
+        }
+
+        public cCategory(int pID, string pCategoryName) {
+            fID = pID;
+            fName = pCategoryName;
+        }
+    }
+}
